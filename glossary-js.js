06b9e16a5db4125efc82b7bd3b8a0d1deec52ac5
Extrarice <extrarice@hotmail.com>
2023-04-01 16:45:39 +0800

    Initial commit

---
+++ glossary-js.js
@@ -0,0 +1,20 @@
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script>
+  $(document).ready(function() {
+    $('form').submit(function(event) {
+      event.preventDefault(); // Prevent the form from submitting
+      var searchTerm = $('#search').val().toLowerCase(); // Get the search term
+      $('dt').each(function() {
+        var term = $(this).text().toLowerCase(); // Get the term for each definition
+        var definition = $(this).next().text(); // Get the definition for each term
+        if (term.indexOf(searchTerm) !== -1) { // Check if the term matches the search term
+          $(this).addClass('match'); // Highlight the matching term
+          $(this).next().addClass('match').attr('aria-live', 'polite'); // Highlight the matching definition and announce it to screen readers
+        } else {
+          $(this).removeClass('match'); // Remove the highlight if it doesn't match
+          $(this).next().removeClass('match').removeAttr('aria-live'); // Remove the highlight and aria-live attribute
+        }
+      });
+    });
+  });
+</script>
